3b0319d95022ca0f5b198c8475c832fdb4038315
Aldevan Silva <aldevan6@gmail.com>
2022-06-14 04:41:34 -0300

    construction of the "our team" section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,34 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:false,
-    autoplay: false,
-    responsive:{
-        0:{
-            items:1
+$('.banner-carrossel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:1
+        1000: {
+            items: 1
         }
     }
 })
+
+$('.slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1015: {
+            items: 1
+        }
+    }
+})
+
+
