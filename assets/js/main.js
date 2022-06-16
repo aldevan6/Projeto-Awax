6e92c459eceb4516e808f76fe6634d8a705b8b7b
Aldevan Silva <aldevan6@gmail.com>
2022-06-16 02:58:44 -0300

    section premium started

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,50 @@ $(document).ready(function () {
         infinite: true,
         dots: true,
         arrows: true,
+        adaptiveHeight: true,
+        autoplay: false,
         slidesToShow: 3,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                    // infinite: true,
+                    // dots: true
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
   });
 });
 
+$(document).ready(function () {
+    $('.section-depositions, .section-premium-area').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        adaptiveHeight: true,
+        autoplay: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+
+    });
+});
