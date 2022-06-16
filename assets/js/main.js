d0a1b101b9de7a769b38b249ad4230a7a1ef3f7d
Aldevan Silva <aldevan6@gmail.com>
2022-06-16 16:01:39 -0300

    Premium section completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-})
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     responsiveClass: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 1
+//         },
+//         1000: {
+//             items: 1
+//         }
+//     }
+// })
 
 $(document).ready(function () {
     $('.section-teams-slides').slick({
@@ -56,7 +56,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('.section-depositions, .section-premium-area').slick({
+    $('.section-depositions, .section-premium-area, .banner-slide').slick({
         infinite: true,
         dots: true,
         arrows: false,
